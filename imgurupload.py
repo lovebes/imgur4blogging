ab0ad698f367494bf6e2bb8f47b0913a2bc99924
lovebes <kseungjin@yahoo.com>
2015-04-12 22:03:43 +0300

    any line with addthumbFILE.EXT will create a thumbnail image at width 600, and HTML tag of link wrapping <IMG> for the original image. of course, both images will be uploaded to imgur.

--- imgurupload.py
+++ imgurupload.py
@@ -29,27 +29,57 @@ def main():
 	folder = args.folder
 	jf = codecs.open(journal, "r", "utf-8")
 	jfw = codecs.open(journal+"_replaced.txt","w", "utf-8")
-	for line in jf:
-		m = re.search('([-\w]+\.(?:JPG|jpg|gif|png))', line)
-		parsedline = line
-		if m:
-			attempt = 0
-			imgpath = folder+m.group(1)
-			imgurl = uploadtoalbum(albumid, imgpath)
-			while False: #imgurl == 'http://failedtoupload.jpg':
-				attempt = attempt + 1
-				print 'failed to upload ' + imgpath + ' will repeat..'
-				print "upload attempt # "+str(attempt)+"\n"
-				time.sleep(3)
-				try: 
-					client = ImgurClient(client_id, client_secret, access_token, refresh_token)
-				except:
-					print "didn't connect. will retry.."
-				imgurl = uploadtoalbum(albumid, imgpath)
-			parsedline = '<img src="'+imgurl+'" width="600"></img>'+'\n'
-			print imgpath+' => '+parsedline
-		else:
-			print parsedline
+    for line in jf:
+        m = re.search('(addthumb)*([-\w]+\.(?:JPG|jpg|gif|png))', line)
+        # m.group(1) should have string 'addthumb' which means 'please make thumbnail and <img> that
+        # thumbnails will resize to 600 width pics, uploaded, and image will be <a href="real"<img src="thumbail"  ~ed.
+        parsedline = line
+        if m:
+            imgpath = folder+m.group(2)
+            #m.group(2) would now have the strings after 'addthumb' modifier, ie the filenames for pics
+            thumbwidth = 600
+            
+            attempt = 0
+            if m.group(1) == 'addthumb':
+                #check image
+                im=Image.open(imgpath) #(width,height) tuple
+                width_org, height_org = im.size
+                thumbheight = int(height_org * thumbwidth / width_org)
+            
+                if  width_org > 600: #only then you would need to make a thumbnail pic
+                    #resize the image and save it with 'thumb' prefix
+                    newimg = im.resize((thumbwidth, thumbheight), Image.ANTIALIAS)
+                    resizepath = folder+'thumb_'+m.group(2)
+                    newimg.save(resizepath)
+                    print("resized file saved as %s" % resizepath)
+                    thumburl = uploadtoalbum(albumid, resizepath)
+                    
+            
+            imgurl = uploadtoalbum(albumid, imgpath)
+            while False: #imgurl == 'http://failedtoupload.jpg':
+                attempt = attempt + 1
+                print 'failed to upload ' + imgpath + ' will repeat..'
+                print "upload attempt # "+str(attempt)+"\n"
+                time.sleep(3)
+                try: 
+                    client = ImgurClient(client_id, client_secret, access_token, refresh_token)
+                except:
+                    print "didn't connect. will retry.."
+                imgurl = uploadtoalbum(albumid, imgpath)
+                if m.group(1) == 'addthumb':
+                    thumburl = uploadtoalbum(albumid, resizepath)
+            
+            if m.group(1) == 'addthumb':
+                parsedline = '<a href="'+imgurl+'" target="new"><img src="'+thumburl+'" width="600"></img></a>'+'\n'
+                print imgpath
+                print ', '+resizepath+' => '+parsedline
+            else:
+                parsedline = '<img src="'+imgurl+'" width="600"></img>'+'\n'
+                print imgpath+' => '+parsedline
+            
+            
+        else:
+            print parsedline
 		jfw.write(parsedline)
 	
 		
